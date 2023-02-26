eb3052abb9ec50e0e1f1cd9fecab0fc2e8da24da
Ajay Sharma <ajshrmaofficial@gmail.com>
2023-02-26 15:16:07 +0530

    order price view at checkout logic

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,24 +1,41 @@
-import { useEffect } from "react"
-import { useNavigate, useLocation, Link } from "react-router-dom"
+import { useEffect, useState } from "react";
+import { useNavigate, Link, useLocation } from "react-router-dom";
+import { server } from "../App";
 
 const Checkout = () => {
-    const navigate = useNavigate()
-    const location = useLocation()
+  const navigate = useNavigate();
+  const location = useLocation();
+  const cartIds = location.state.cart;
+  const [orderPrice, setOrderPrice] = useState(0);
+  useEffect(() => {
+    server
+      .post("/cal", { id: cartIds })
+      .then((res) => {
+        setOrderPrice(res.data.totalPrice);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    const {orderPrice} = location.state
-    useEffect(()=>{
-        setTimeout(()=>{
-            navigate('/')
-        }, 5000)
-    },[])
-    
-    return(
-        <div className="checkoutPage">
-            <h1>Congratulations🎉<br/> Your order of ₹{orderPrice} has been placed.</h1>
-            <i>You will be redirected to the home page in 5 seconds.</i>
-            <Link className="btn" to='/'>Click here to go back to the home page.</Link>
-        </div>
-    )
-}
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/");
+    }, 5000);
+  }, [orderPrice]);
 
-export default Checkout
+  return (
+    <div className="checkoutPage">
+      <h1>
+        Congratulations🎉
+        <br /> Your order of ₹{orderPrice} has been placed.
+      </h1>
+      <i>You will be redirected to the home page in 5 seconds.</i>
+      <Link className="btn" to="/">
+        Click here to go back to the home page.
+      </Link>
+    </div>
+  );
+};
+
+export default Checkout;
